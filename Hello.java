94434c2f0144bc414edeb7f2e30062dfd106109a
duynnsoftware <nguyenngocduy81@gmail.com>
2016-08-20 11:32:32 +0700

    modify Hello.java and README.md

--- Hello.java
+++ Hello.java
@@ -1,17 +1,19 @@
 public class Hello{
 	public static void main(String[] ar){
 		System.out.println("Hello kiu kiu");
-		Student stu=new Student("Cuong");
+		Student stu=new Student("Cuong", "Ha Noi");
 		stu.show();
 	}
 }
 class Student{
 	private String name;
-	public Student(String name){
+	private String addr;
+	public Student(String name, String addr){
 		this.name=name;
+		this.addr=addr;
 	}
 
 	public void show(){
-		System.out.println("Hello " + this.name);
+		System.out.println("Hello " + this.name+", address: " + addr);
 	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# hello
+Duy1981 lay tu Duy81 ve va repare, hehe
